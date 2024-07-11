ed4dfb021c5ed82e883f85b764275260187cecac
dolly.004 <dai.chen.22@ucl.ac.uk>
2024-07-11 13:08:07 +0100

    added seed to step_evaluation,ipynb, added intermediate image saving functions

--- hackathon_starter_kit/src/sampling/dmps.py
+++ hackathon_starter_kit/src/sampling/dmps.py
@@ -2,6 +2,9 @@
 from typing import Tuple
 import torch
 from sampling.epsilon_net import ddim_step, EpsilonNet
+import numpy as np
+from PIL import Image
+import os
 
 
 def dpms(
@@ -33,6 +36,74 @@ def dpms(
 
     sample = initial_noise
     for i in range(len(epsilon_net.timesteps) - 1, 1, -1):
+        t, t_prev = epsilon_net.timesteps[i], epsilon_net.timesteps[i - 1]
+        sample.requires_grad_()
+        e_t = epsilon_net.predict_x0(sample, t)
+        ### NEW
+        grad_value, alpha_t = epsilon_net.approximate_grad_log_likelihood(x_t = sample, t=t_prev, H_funcs=H_func, y=obs, noise_std=std)
+        ###
+
+        sample = ddim_step(
+            x=sample, epsilon_net=epsilon_net, t=t, t_prev=t_prev, eta=eta, e_t=e_t
+        ).detach()
+
+        # gradient step
+        sample = sample + lam * grad_value * (1-alpha_t)/torch.sqrt(alpha_t)
+
+    # last diffusion step
+    sample.requires_grad_()
+    grad_value, alpha_t = epsilon_net.approximate_grad_log_likelihood(x_t = sample, t=1, H_funcs=H_func, y=obs, noise_std=std)
+
+    sample = epsilon_net.predict_x0(sample, epsilon_net.timesteps[1]) + lam * grad_value * (1-alpha_t)/torch.sqrt(alpha_t)
+
+    return sample.detach()
+
+def dpms_save(
+    initial_noise: torch.Tensor,
+    inverse_problem: Tuple,
+    epsilon_net: EpsilonNet,
+    lam: float = 1.0,
+    gamma: float = 1.0,
+    eta: float = 1.0,
+    output_path: str = None,
+    interval = 10
+):
+    """Use DPMS algorithm to solve an inverse problem.
+
+    This is an implementation of [1].
+    Use ``gamma`` to control the strength of the gradient perturbation.
+
+    Note
+    ----
+    - Use ``initial_noise`` to set the number of samples ``(n_samples, *shape_of_data)``.
+
+    References
+    ----------
+    .. Meng, Xiangming, and Yoshiyuki Kabashima. "Diffusion model based posterior sampling 
+    for noisy linear inverse problems." arXiv preprint arXiv:2211.12343 (2022).
+
+    """
+    obs, H_func, std = inverse_problem
+    A = H_func.H
+    shape = (initial_noise.shape[0], *(1,) * len(initial_noise.shape[1:]))
+
+    sample = initial_noise
+    for i in range(len(epsilon_net.timesteps) - 1, 1, -1):
+        # save image every interval steps
+        # make the progress folder
+        if not os.path.exists(os.path.join(output_path, "progress")):
+            os.makedirs(os.path.join(output_path, "progress"))
+        if i % interval == 0:
+
+            sample_i = sample.squeeze(0).detach().cpu().permute(1, 2, 0)
+
+            sample_i = sample_i.clamp(-1, 1)
+            sample_i = (sample_i + 1.0) * 127.5
+            sample_i = sample_i.numpy().astype(np.uint8)
+            img_pil = Image.fromarray(sample_i)
+            #save image
+            img_pil.save(os.path.join(output_path, f"progress/output_{str(i).zfill(4)}.png"))
+
         t, t_prev = epsilon_net.timesteps[i], epsilon_net.timesteps[i - 1]
         sample.requires_grad_()
         e_t = epsilon_net.predict_x0(sample, t)
--- hackathon_starter_kit/src/sampling/dps.py
+++ hackathon_starter_kit/src/sampling/dps.py
@@ -1,6 +1,10 @@
 from typing import Tuple
 import torch
 from sampling.epsilon_net import ddim_step, EpsilonNet
+import numpy as np
+from PIL import Image
+import os
+
 
 
 def dps(
@@ -52,6 +56,88 @@ def dps(
         # gradient step
         grad_pot = gamma * grad_pot / grad_norm
         sample = sample + grad_pot
+
+    # last diffusion step
+    sample.requires_grad_()
+    grad_lklhd = torch.autograd.grad(pot_func(sample), sample)[0]
+    grad_norm = error(sample).reshape(*shape)
+    grad_lklhd = (gamma / grad_norm) * grad_lklhd
+
+    sample = epsilon_net.predict_x0(sample, epsilon_net.timesteps[1]) + grad_lklhd
+
+    return sample.detach()
+
+
+def dps_save(
+    initial_noise: torch.Tensor,
+    inverse_problem: Tuple,
+    epsilon_net: EpsilonNet,
+    gamma: float = 1.0,
+    eta: float = 1.0,
+    output_path: str = None,
+    interval = 10
+):
+    """Use DPS algorithm to solve an inverse problem.
+
+    This is an implementation of [1].
+    Use ``gamma`` to control the strength of the gradient perturbation.
+
+    Note
+    ----
+    - Use ``initial_noise`` to set the number of samples ``(n_samples, *shape_of_data)``.
+
+    References
+    ----------
+    .. [1] Chung, Hyungjin, et al. "Diffusion posterior sampling for general noisy inverse problems."
+    arXiv preprint arXiv:2209.14687 (2022).
+
+    """
+    obs, H_func, std = inverse_problem
+    A = H_func.H
+    shape = (initial_noise.shape[0], *(1,) * len(initial_noise.shape[1:]))
+
+    def pot_func(x):
+        return -torch.norm(obs.reshape(1, -1) - A(x)) ** 2.0
+
+    def error(x):
+        return torch.norm(obs.reshape(1, -1) - A(x), dim=-1)
+
+    sample = initial_noise
+    for i in range(len(epsilon_net.timesteps) - 1, 1, -1):
+        # save image every interval steps
+        # make the progress folder
+        if not os.path.exists(os.path.join(output_path, "progress")):
+            os.makedirs(os.path.join(output_path, "progress"))
+        if i % interval == 0:
+
+            sample_i = sample.squeeze(0).detach().cpu().permute(1, 2, 0)
+
+            sample_i = sample_i.clamp(-1, 1)
+            sample_i = (sample_i + 1.0) * 127.5
+            sample_i = sample_i.numpy().astype(np.uint8)
+            img_pil = Image.fromarray(sample_i)
+            #save image
+            img_pil.save(os.path.join(output_path, f"progress/output_{str(i).zfill(4)}.png"))
+            
+        t, t_prev = epsilon_net.timesteps[i], epsilon_net.timesteps[i - 1]
+
+        sample.requires_grad_()
+        e_t = epsilon_net.predict_x0(sample, t)
+        grad_norm = error(e_t).reshape(*shape)
+        pot_val = pot_func(e_t)
+        grad_pot = torch.autograd.grad(pot_val, sample)[0]
+
+        sample = ddim_step(
+            x=sample, epsilon_net=epsilon_net, t=t, t_prev=t_prev, eta=eta, e_t=e_t
+        ).detach()
+
+
+
+        # gradient step
+        grad_pot = gamma * grad_pot / grad_norm
+        sample = sample + grad_pot
+
+
 
     # last diffusion step
     sample.requires_grad_()
